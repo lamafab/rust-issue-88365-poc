171b23132770b8207fb3756bcfdf56a8f8e534d9
lamafab <42901763+lamafab@users.noreply.github.com>
2021-08-27 00:16:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Reproducible panic for Rust issue [#88365](https://github.com/rust-lang/rust/issues/88365)
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![feature(const_generics)]
+
+fn main() {
+    use core::ops::Index;
+
+    trait MyTrait {
+        type ArrayType: Index<usize, Output = u8>;
+
+        const SIZE: usize;
+        const ARRAY: Self::ArrayType;
+    }
+
+    struct MyStruct;
+
+    impl MyTrait for MyStruct {
+        type ArrayType = [u8; Self::SIZE];
+
+        const SIZE: usize = 4;
+        const ARRAY: [u8; Self::SIZE] = [1, 2, 3, 4];
+    }
+
+    let res = <MyStruct as MyTrait>::ARRAY[2]; // PANICS HERE
+    assert_eq!(res, 3);
+}
